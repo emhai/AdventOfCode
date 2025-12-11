db1f89572f056a2915edd44648ec82a9adc05892
emmahaidacher <emma.haidacher@hotmail.com>
2025-12-11 16:10:15 +0100

    recursion try. too long

--- 2025/day11/main.py
+++ 2025/day11/main.py
@@ -1,18 +1,69 @@
+import copy
 import os
 
 def get_content(file_name):
     f = open(file_name)
     content = f.read().splitlines()
+    input = {}
+    for c in content:
+        device, outs = c.split(":")
+        outs = outs.strip()
+        outs = outs.split(" ")
+        input[device] = outs
     f.close()
-    return content
+    return input
+
+# 113037178808 too high
+def rec_1(input, content, counter):
+    if input == "out":
+        return 1
+    else:
+        total = 0
+        for node in content[input]:
+            if node == "you":
+                continue
+            total += rec_1(node, content, counter)
+        return total
+
 
 def part_1(content):
 
-    return
+    counter = 0
+    for node in content["you"]:
+        counter += rec_1(node, content, counter)
+
+    print(counter)
+
+def rec_2(input, content, path, paths):
+    if input == "out":
+        if "dac" in path or "fft" in path:
+            print(path)
+            return path
+    else:
+        for node in content[input]:
+            if node in path:
+                continue
+            ind_path = copy.deepcopy(path)
+            ind_path.append(node)
+            f = rec_2(node, content, copy.deepcopy(ind_path), paths)
+            paths.append(f)
 
 def part_2(content):
 
-    return
+    paths = []
+    rec_2("svr", content, ["svr"], paths)
+    counter = 0
+    for el in paths:
+        if el is None:
+            continue
+        print(el)
+
+        if "dac" in el and "fft" in el:
+            print("true")
+            counter += 1
+
+
+    return counter
 
 def main():
     content = get_content("input_test.txt")
